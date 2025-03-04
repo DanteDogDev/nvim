c4f572d2667946f471bbc2ac46f421861d1ece10
Dante Harper <dantedigdog@gmail.com>
2025-03-04 11:47:06 +0100

    dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -14,6 +14,11 @@ return {
       preset = {
         header = logo,
       },
+      sections = {
+        {section = "header"},
+        {section = "startup", padding = 1},
+        {icon = " ", title = "Git Status", section = "terminal", enabled = function() return Snacks.git.get_root() ~= nil end, cmd = "git status --short --branch --renames", height = 5, padding = 1, ttl = 5 * 60, indent = 3, },
+      }
     },
   },
 }
--- lua/scripts/cmake/cmake-terminal.lua
+++ lua/scripts/cmake/cmake-terminal.lua
@@ -1,22 +1,4 @@
 local Terminal = {}
-local function ApplySyntax(win)
-  vim.fn.matchadd("@constructor", "[0-9]",100, -1, {window = win})
-  vim.fn.matchadd("@comment.error", "\\verror:.*", 100, -1, {window = win})
-  vim.fn.matchadd("@comment.error", "error\\w", 100, -1, {window = win})
-  vim.fn.matchadd("@comment.warning", "\\vwarning:.*", 100, -1, {window = win})
-  vim.fn.matchadd("@comment.warning", "warning\\w", 100, -1, {window = win})
-  vim.fn.matchadd("@comment.note", "\\vnote:.*", 100, -1, {window = win})
-  vim.fn.matchadd("@keyword", "\\[.*\\]",100, -1, {window = win})
-  vim.fn.matchadd("@constructor", "\\[-.*\\]",100, -1, {window = win})
-  vim.fn.matchadd("@annotation", "\\[\\[.*\\]\\]",100, -1, {window = win})
-  vim.fn.matchadd("String", '"[^"]*"',100, -1, {window = win})
-  vim.fn.matchadd("String", "'[^']*'",100, -1, {window = win})
-  vim.fn.matchadd("Directory", "\\zs[A-Z]:\\(.*\\)(\\d\\+,\\d\\+):",100, -1, {window = win})
-  vim.fn.matchadd("@comment", "\\zs|.*$",100, -1, {window = win})
-  vim.fn.matchadd("none", "\\zs\\^\\(.*\\)",100, -1, {window = win})
-  vim.fn.matchadd("conceal", "\\zsIn file included from \\(.*\\)",100, -1, {window = win})
-end
-
 Terminal.Call =  function(cmd, syntax)
   if Terminal.job then
     print("Process is Already Running")
@@ -31,7 +13,7 @@ Terminal.Call =  function(cmd, syntax)
     row = 2,
     border = 'single'
   })
-  ApplySyntax(win)
+  require("scripts.cmake.cmake-syntax").ApplySyntax(win)
   local function on_stdout(_, data)
     if data then
       local cleaned_data = vim.fn.trim(table.concat(data, '\n'))
