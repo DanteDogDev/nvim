1c91f31d32452a8ee54c3c8a0521bdd2668307a6
Dante Harper <dantedigdog@gmail.com>
2025-03-14 22:19:01 +0100

    compiler sorta works but not

--- lua/scripts/compile-tools/compilers/cmake.lua
+++ lua/scripts/compile-tools/compilers/cmake.lua
@@ -1,12 +1,80 @@
-local M = {}
+local M = {
+  json = {
+    module = "cmake"
+  },
+}
+local function select_build_type()
+  vim.ui.select({ "debug", "release" }, { prompt = "Select Build Type: " }, function(build_type)
+    M.json.build_type = build_type
+    require("scripts.compile-tools").json.encode_project(M.json)
+  end)
+end
+local function select_kit()
+  local cmake = require("scripts.compile-tools").json.decode_module("cmake")
+  if type(cmake) ~= "table" then
+    return
+  end
+  local names = {}
+  for _, item in ipairs(cmake) do
+    table.insert(names, item.name)
+  end
+  vim.ui.select(names, { prompt = "Select CMake kit: " }, function(kit)
+    for _, item in ipairs(cmake) do
+      if item.name == kit then
+        M.json.settings = item
+        select_build_type()
+        return
+      end
+    end
+  end)
+end
+M.setup = function()
+  select_kit()
+end
 M.clean = function()
+  vim.fn.delete("bin", "rf")
 end
 M.reload = function()
+  select_build_type()
 end
 M.generate = function()
+  local json = require("scripts.compile-tools").json.decode_project()
+  if not json then return end
+  local dir = "./bin/" .. json.build_type
+  local terminal = require("scripts.compile-tools").terminal
+  if json.settings.envSetupScript then
+    terminal.send_command(json.settings.envSetupScript, {}, dir)
+  end
+  local args = {} ---@type string[]
+  if json.settings.generator then
+    table.insert(args, '-G"' .. json.settings.generator .. '"')
+  end
+  if json.build_type == "debug" then
+    table.insert(args, "-DCMAKE_BUILD_TYPE=Debug")
+  elseif json.build_type == "release" then
+    table.insert(args, "-DCMAKE_BUILD_TYPE=Release")
+  end
+  if json.settings.compilers then
+    if json.settings.compilers.C then
+      table.insert(args, '-DCMAKE_C_COMPILER="' .. json.settings.compilers.C .. '"')
+    end
+    if json.settings.compilers.CXX then
+      table.insert(args, '-DCMAKE_CXX_COMPILER="' .. json.settings.compilers.CXX .. '"')
+    end
+  end
+  if json.settings.toolchainFile then
+    table.insert(args, '-DCMAKE_TOOLCHAIN_FILE="' .. json.settings.toolchainFile .. '"')
+  end
+
+  table.insert(args, '-B "' .. dir .. '"')
+  terminal.send_command("cmake", args)
 end
 M.build = function()
+  local json = require("scripts.compile-tools").json.decode_project()
+  if not json then return end
+  local dir = "./bin/" .. json.build_type
+  local terminal = require("scripts.compile-tools").terminal
+  terminal.send_command("cmake", {"--build"}, dir)
 end
-M.run = function()
-end
+M.run = function() end
 return M
--- lua/scripts/compile-tools/init.lua
+++ lua/scripts/compile-tools/init.lua
@@ -1,8 +1,9 @@
-local M = { init = false}
+local M = { init = false }
 M.setup = function(opts)
   opts = opts or {}
-  if M.init then return end
-  opts.self = M
+  if M.init then
+    return
+  end
   M.init = true
   M.json = require("scripts.compile-tools.json")
   M.json.setup(opts)
@@ -10,17 +11,85 @@ M.setup = function(opts)
   M.terminal.setup(opts)
   M.syntax = require("scripts.compile-tools.syntax")
   M.syntax.setup(opts)
+  M.module = nil
+
+  vim.api.nvim_create_user_command("CompileTools", function(opts)
+    local command = opts.fargs[1]
+    if command == "clean" then
+    elseif command == "reload" then
+    elseif command == "generate" then
+    elseif command == "build" then
+    elseif command == "run" then
+    elseif command == "toggle_term" then
+      M.terminal.toggle_terminal()
+    elseif command == "clear_term" then
+      M.terminal.clear_terminal()
+    elseif command == "command" then
+      vim.ui.input({ prompt = "Enter command: " }, function(input)
+        if input and input ~= "" then
+          local cmd, args = input:match("^(%S+)%s*(.*)")
+          M.terminal.send_command(cmd, vim.fn.split(args, "%s+"))
+        else
+          print("No command entered")
+        end
+      end)
+    end
+  end, {
+    nargs = 1,
+    complete = function()
+      return { "clean", "reload", "generate", "build", "run", "command", "toggle_term", "clear_term" }
+    end,
+  })
+end
+M.load = function()
+  if M.module then
+    return
+  end
+  local project = M.json.decode_project()
+  if not project then
+    local dir = vim.fn.stdpath("config") .. "/lua/scripts/compile-tools/compilers"
+    local files = vim.fn.globpath(dir, "*", false, true)
+    local modules = {}
+    for _, file in ipairs(files) do
+      local filename = vim.fn.fnamemodify(file, ":t"):match("^(.-)%.") or vim.fn.fnamemodify(file, ":t")
+      table.insert(modules, filename)
+    end
+    local json = {}
+    vim.ui.select(modules, { prompt = "Select Compiler Module: " }, function(module)
+      json.module = module
+      require("scripts.compile-tools").json.encode_project(json)
+      M.module = require("scripts.compile-tools.compilers." .. module)
+      M.module.setup()
+    end)
+  else
+    M.module = require("scripts.compile-tools.compilers." .. project.module)
+  end
 end
 M.clean = function()
+  if not M.json.decode_project() then return end
+  if not M.module then M.load() end
+  M.module = nil
+  M.module.clean()
 end
 M.reload = function()
+  if not M.json.decode_project() then return end
+  if not M.module then M.load() end
+  M.module.reload()
 end
 M.generate = function()
+  if not M.json.decode_project() then return end
+  if not M.module then M.load() end
+  M.module.generate()
 end
+
 M.build = function()
+  if not M.json.decode_project() then return end
+  if not M.module then M.load() end
+  M.module.build()
 end
 M.run = function()
-end
-M.build_and_run = function()
+  if not M.json.decode_project() then return end
+  if not M.module then M.load() end
+  M.module.run()
 end
 return M
