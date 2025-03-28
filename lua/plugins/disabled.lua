9b31d164a1cb285ccd1ff0a22214d45da4a94eae
Dante Harper <dantedigdog@gmail.com>
2025-03-28 11:49:28 +0100

    fixed :3

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,8 +2,10 @@ return {
   { "Civitasv/cmake-tools.nvim", enabled = false }, -- not worthbbb
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      opts.diagnostics.virtual_text = false
-    end,
+    opts = {
+      diagnostics = {
+        virtual_text = false,
+      },
+    },
   },
 }
